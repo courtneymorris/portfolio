fce0a4db4c9de5de48b7572abcffa97c031afc66
courtneymorris <shull.courtney@gmail.com>
2024-09-05 15:14:03 -0600

    Initial commit

---
+++ src/components/footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "../styles/Footer.module.scss";
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div>
+        Uicons by <a href="https://www.flaticon.com/uicons">Flaticon</a>
+      </div>
+      <div>Graphics by Courtney Morris</div>
+    </footer>
+  );
+}
